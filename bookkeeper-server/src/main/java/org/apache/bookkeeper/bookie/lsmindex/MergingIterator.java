9fd8c9a09ee407a7baf52dadf2881c7360da64f6
Ivan Kelly <ivank@yahoo-inc.com>
2013-08-28 17:45:38 +0200

    Merging iterator

---
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/lsmindex/MergingIterator.java
@@ -0,0 +1,96 @@
+package org.apache.bookkeeper.bookie.lsmindex;
+
+import org.apache.bookkeeper.proto.DataFormats.KeyValue;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import com.google.protobuf.ByteString;
+
+class MergingIterator implements KeyValueIterator {
+    final List<KeyValueIterator> origOrder;
+    final KeyValueIterator[] iterators;
+    final Comparator<ByteString> keyComparator;
+    
+    boolean sorted = false;
+    IOException sortError = null;
+   
+    final Comparator<KeyValueIterator> iteratorComp = new Comparator<KeyValueIterator>() {
+        @Override
+        public int compare(KeyValueIterator o1, KeyValueIterator o2) {
+            try {
+                if (!o1.hasNext()) {
+                    return 1;
+                } else if (!o2.hasNext()) {
+                    return -1;
+                }
+                int diff = keyComparator.compare(o1.peek().getKey(), o2.peek().getKey());
+                if (diff == 0) {
+                    return origOrder.indexOf(o1) - origOrder.indexOf(o2);
+                } else {
+                    return diff;
+                }
+            } catch (IOException ioe) {
+                if (sortError == null) {
+                    sortError = ioe;
+                }
+                return 0;
+            }
+        }
+    };
+
+    /**
+     * Create an iterator that iterates over the entries of all iterators in the key
+     * order determined by keyComparator.
+     * If two keys match, then the keys will be merged in the order that the iterator
+     * that contains them was passed to the MergingIterator constructor
+     */
+    MergingIterator(Comparator<ByteString> keyComparator, KeyValueIterator... iterators) {
+        if (iterators.length < 1) {
+            throw new IllegalArgumentException("Must pass at least one iterator");
+        }
+        this.origOrder = Arrays.asList(Arrays.copyOf(iterators, iterators.length));
+        this.iterators = iterators;
+        this.keyComparator = keyComparator;
+    }
+
+    @Override
+    public synchronized boolean hasNext() throws IOException {
+        if (!sorted) {
+            Arrays.sort(iterators, iteratorComp);
+            if (sortError != null) {
+                throw sortError;
+            }
+            sorted = true;
+        }
+
+        return iterators[0].hasNext();
+    }
+
+    @Override
+    public synchronized KeyValue peek() throws IOException {
+        if (!hasNext()) {
+            throw new IOException("No next entry");
+        }
+        return iterators[0].peek();
+    }
+
+    @Override
+    public synchronized KeyValue next() throws IOException {
+        if (!hasNext()) {
+            throw new IOException("No next entry");
+        }
+        sorted = false;
+        return iterators[0].next();
+    }
+
+    @Override
+    public void close() throws IOException {
+        for (KeyValueIterator i : iterators) {
+            i.close();
+        }
+    }
+
+}
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/lsmindex/TestMergingIterator.java
@@ -0,0 +1,135 @@
+package org.apache.bookkeeper.bookie.lsmindex;
+
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.io.File;
+import com.google.common.primitives.Ints;
+import org.apache.bookkeeper.proto.DataFormats.KeyValue;
+import com.google.protobuf.ByteString;
+import com.google.common.primitives.UnsignedBytes;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMergingIterator {
+    private final static Logger LOG = LoggerFactory.getLogger(TestMergingIterator.class);
+
+    Comparator<ByteString> keyComp = new Comparator<ByteString>() {
+        Comparator<byte[]> byteComp = UnsignedBytes.lexicographicalComparator();
+
+        @Override
+        public int compare(ByteString k1, ByteString k2) {
+            return byteComp.compare(k1.toByteArray(), k2.toByteArray());
+        }
+    };
+
+    @Test
+    public void testMerge() throws Exception {
+        List<List<KeyValue>> values = new ArrayList<List<KeyValue>>();
+        values.add(new ArrayList<KeyValue>());
+        values.add(new ArrayList<KeyValue>());
+        values.add(new ArrayList<KeyValue>());
+
+        int NUM_ENTRIES = 1000;
+        Random r = new Random(0xdeadbeef);
+        for (int i = 0; i < NUM_ENTRIES; i++) {
+            values.get(r.nextInt(values.size()))
+                .add(KeyValue.newBuilder()
+                     .setKey(ByteString.copyFrom(Ints.toByteArray(i)))
+                     .setValue(ByteString.copyFrom(Ints.toByteArray(i))).build());
+        }
+
+        MergingIterator iter = new MergingIterator(keyComp,
+                new KeyValueIteratorWrapper(values.get(0).iterator()),
+                new KeyValueIteratorWrapper(values.get(1).iterator()),
+                new KeyValueIteratorWrapper(values.get(2).iterator()));
+        int i = 0;
+        ByteString lastKey = null;
+        while (iter.hasNext()) {
+            i++;
+            KeyValue kv = iter.next();
+
+            if (lastKey != null) {
+                Assert.assertTrue("Should be in order", keyComp.compare(lastKey, kv.getKey()) < 0);
+            }
+            lastKey = kv.getKey();
+        }
+        Assert.assertEquals("All entries should be accounted for", NUM_ENTRIES, i);
+    }
+
+    @Test
+    public void testDupes() throws Exception {
+        List<List<KeyValue>> values = new ArrayList<List<KeyValue>>();
+        values.add(new ArrayList<KeyValue>());
+        values.add(new ArrayList<KeyValue>());
+        values.add(new ArrayList<KeyValue>());
+
+        int NUM_ENTRIES = 1000;
+        Random r = new Random(0xdeadbeef);
+        int entriesAdded = 0;
+        for (int i = 0; i < NUM_ENTRIES; i++) {
+            if ((i % 13) == 0) { // put in 2
+                int which = r.nextInt(values.size());
+                values.get(which)
+                    .add(KeyValue.newBuilder()
+                         .setKey(ByteString.copyFrom(Ints.toByteArray(i)))
+                         .setValue(ByteString.copyFrom(Ints.toByteArray(which))).build());
+                which = (which + 1) % values.size();
+                values.get(which)
+                    .add(KeyValue.newBuilder()
+                         .setKey(ByteString.copyFrom(Ints.toByteArray(i)))
+                         .setValue(ByteString.copyFrom(Ints.toByteArray(which))).build());
+                entriesAdded += 2;
+            } else if ((i % 11) == 0) { // put in all 3
+                int which = r.nextInt(values.size());
+                for (int j = 0; j < values.size(); j++) {
+                    which = (which + 1) % values.size();
+                    values.get(which)
+                        .add(KeyValue.newBuilder()
+                             .setKey(ByteString.copyFrom(Ints.toByteArray(i)))
+                             .setValue(ByteString.copyFrom(Ints.toByteArray(which))).build());
+                    entriesAdded++;
+                }                
+            } else {
+                values.get(r.nextInt(values.size()))
+                    .add(KeyValue.newBuilder()
+                         .setKey(ByteString.copyFrom(Ints.toByteArray(i)))
+                         .setValue(ByteString.copyFrom(Ints.toByteArray(i))).build());
+                entriesAdded++;
+            }
+        }
+
+        MergingIterator iter = new MergingIterator(keyComp,
+                new KeyValueIteratorWrapper(values.get(0).iterator()),
+                new KeyValueIteratorWrapper(values.get(1).iterator()),
+                new KeyValueIteratorWrapper(values.get(2).iterator()));
+        int i = 0;
+        KeyValue lastKV = null;
+        while (iter.hasNext()) {
+            i++;
+            KeyValue kv = iter.next();
+
+            if (lastKV != null) {
+                if (keyComp.compare(lastKV.getKey(), kv.getKey()) == 0) {
+                    int lastVal = Ints.fromByteArray(lastKV.getValue().toByteArray());
+                    int curVal = Ints.fromByteArray(kv.getValue().toByteArray());
+                    Assert.assertTrue("Should be in the order of the iterators", lastVal < curVal);
+                } else {
+                    Assert.assertTrue("Should be in order",
+                                      keyComp.compare(lastKV.getKey(), kv.getKey()) < 0);
+                }
+            }
+            lastKV = kv;
+        }
+        Assert.assertEquals("All entries should be accounted for", entriesAdded, i);
+    }
+}
